230b41689f8c223139ab362318ee1d14dd56e032
sathesbitmaker <sathes352@gmail.com>
2021-02-14 12:31:54 -0500

    JavaScriptPlaying

---
+++ script.js
@@ -0,0 +1,9 @@
+// function appear() {
+//     document.getElementById("test").style.opacity = "1";
+// }
+
+window.onload=function(){
+var myP = document.getElementById('test');
+
+myP.addEventListener('mouseover', function(){ myP.style.opacity = "1"})
+}
