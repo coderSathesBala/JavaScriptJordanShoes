5c101591de6a0ea526cda3e5d2119a412dcddfa3
sathesbitmaker <sathes352@gmail.com>
2021-02-14 12:33:54 -0500

    AddAOS

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
+import AOS from 'aos';
+import 'aos/dist/aos.css'; // You can also use <link> for styles
+// ..
+AOS.init();
+
 // function appear() {
 //     document.getElementById("test").style.opacity = "1";
 // }
@@ -7,3 +12,4 @@ var myP = document.getElementById('test');
 
 myP.addEventListener('mouseover', function(){ myP.style.opacity = "1"})
 }
+
